85c02120abdbf1fb4a06cf73a4b5b82bf7752e43
xaver <xaver.maierhofer@xwissen.info>
2023-06-29 13:29:21 +0200

    Change commands to mariadb & support SQL file in restore

--- .docker-mate/utils/mysql-restore.sh
+++ .docker-mate/utils/mysql-restore.sh
@@ -5,9 +5,13 @@ if ! docker ps -q --no-trunc | grep -q "$(docker compose ps -q db)"; then
     exit
 fi
 
+if [[ -f "$1" ]]; then
+    zcat "$1" | docker exec -i "$(docker compose ps -q db)" mariadb -u "${DB_USER}" -p"${DB_PASSWORD}" "${DB_NAME}"
+fi
+
 if [[ -f "./backup/mysql.sql.gz" ]]; then
     bash .docker-mate/utils/message.sh info "Restoring ./backup/mysql.sql.gz"
-    zcat "backup/mysql.sql.gz" | docker exec -i "$(docker compose ps -q db)" mysql -u "${DB_USER}" -p"${DB_PASSWORD}" "${DB_NAME}"
+    zcat "backup/mysql.sql.gz" | docker exec -i "$(docker compose ps -q db)" mariadb -u "${DB_USER}" -p"${DB_PASSWORD}" "${DB_NAME}"
     bash .docker-mate/utils/message.sh success "Restore successful!"
 else
     bash .docker-mate/utils/message.sh info "No backup found: backup/mysql.sql.gz"
